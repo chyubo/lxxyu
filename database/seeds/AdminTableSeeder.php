eec86f4aae5055e8782348dfae7846222a93da75
chyubo <chyubo@foxmail.com>
2018-01-24 16:50:38 +0800

    no message

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = \Faker\Factory::create('zh_CN');
+        for($i = 0; $i < 5 ; $i++){
+        	DB::table('admin') -> insert([
+        			'username' => $faker -> username(20),
+        			'password' => bcrypt('123456'),
+        			'gender'   => rand(1,3),
+        			'mobile'   => $faker -> phoneNumber,
+        			'email'    => $faker -> email(40),
+        			'role_id'  => rand(1,5),
+        			'created_at'=> date('Y-m-d H:i:s'),
+        			'status'   => rand(1,2)
+        		]);
+        }
+    }
+}
